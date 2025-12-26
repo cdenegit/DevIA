d1e6704e8a16a3949929910ec6844036e8c201ca
cdenegit <168839647+cdenegit@users.noreply.github.com>
2025-12-26 15:37:02 -0500

    Update api01.py

--- api01.py
+++ api01.py
@@ -391,10 +391,13 @@ def diagnostico_indice(indice, nombre):
     resumen_web = f"Estado general: {resumen.split('.')[0]}."
 
     return {
-        "avg": avg,                              # ← ahora disponible para UI / PDF
-        "diagnostico_detallado": diagnostico,    # PDF
-        "resumen_web": resumen_web,              # Web
-        "color": color                           # semáforo
+        "diagnostico_detallado": diagnostico,     # PDF
+        "resumen_web": resumen_web,               # Web
+        "color": color,                           # semáforo
+        "valor": avg,                             # ← ahora disponible para UI / PDF
+        "area_sana": area_sana,
+        "area_media": area_media,
+        "area_estres": area_estres
     }
 
 # =====================================
