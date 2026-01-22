6ad11e000016661d8e14e052164baf6f9be8a074
cdenegit <168839647+cdenegit@users.noreply.github.com>
2026-01-22 14:58:03 -0500

    Update api01.py

--- api01.py
+++ api01.py
@@ -750,6 +750,8 @@ def generar_rgb_con_geojson(arr01, geom):
     arr01: ndarray HxW or HxWx3 normalizado (0..1)
     geom: shapely Polygon en lon/lat
     """
+
+    print(">>> USANDO BOUNDS: ", geom.bounds, "IMG SHAPE:", arr01.shape)
     h, w = arr01.shape[:2]
     minx, miny, maxx, maxy = geom.bounds
 
